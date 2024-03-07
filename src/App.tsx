89fda520e69995e663b34e7a2a80e263b0294ff2
boytrend0108 <boytrend@gmail.com>
2024-03-07 19:21:30 +0200

    add: MyLoader comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
-import { useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import './styles/App.scss';
-import { Post } from './types/post';
-import { PostList } from './components/PostList';
 import { Form } from './components/Form';
+import { PostList } from './components/PostList';
 import { PostFilter } from './components/PostFilter';
-import { Filters } from './types/filter';
 import { MyModal } from './components/UI/modal/MyModal';
 import { MyButton } from './components/UI/button/MyButton';
 import { usePosts } from './hooks/usePost';
+import { Post } from './types/post';
+import { Filters } from './types/filter';
+import PostService from './api/PostServise';
+import { MyLoader } from './components/UI/loader/MyLoader';
 
 function App() {
   const [posts, setPosts] = useState<Post[]>([]);
   const [filter, setFilter] = useState<Filters>({ sort: '', query: '' });
   const [modal, setModal] = useState(false);
+  const [isPostsLoading, setIsPostsLoading] = useState(true);
 
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+
+  useEffect(() => {
+    fetchPost();
+  }, []);
+
+  const fetchPost = async () => {
+    const posts = await PostService.getAll();
+   
+    setPosts(posts);
+    setIsPostsLoading(false);
+  }
 
   function handleOnSubmit(post: Post) {
     setPosts(currentPosts => [...currentPosts, post])
@@ -37,8 +51,14 @@ function App() {
       <h1 className="app__title">Post List</h1>
 
       <PostFilter setFilter={setFilter} filter={filter} />
-
-      <PostList posts={sortedAndSearchedPosts} remove={removePost} />
+      
+      {isPostsLoading 
+        ?  <div className='app__loader'>
+             <MyLoader />
+          </div>
+        :  <PostList posts={sortedAndSearchedPosts} remove={removePost} />
+      }
+     
     </div>
   );
 }
---
+++ src/components/UI/loader/MyLoader.tsx
@@ -0,0 +1,9 @@
+import cl from './MyLoader.module.scss';
+
+export const MyLoader = () => {
+  return (
+    <div className={cl.MyLoader}>
+
+    </div>
+  )
+}
